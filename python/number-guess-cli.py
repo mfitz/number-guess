5bd1c3dbb4e78a23efe4726e9dcb2a8661013d74
Michael Fitzmaurice <michael@michaelfitzmaurice.com>
2016-03-19 12:34:13 +0000

    Added a python command line implementation

---
+++ python/number-guess-cli.py
@@ -0,0 +1,14 @@
+import sys, random
+
+upperBound = int(sys.argv[1]) if len(sys.argv) > 1 else 100
+numberToGuess = random.randint(1, upperBound)
+guess = -1
+numberOfGuesses = 0
+prompt = "Pick a number between 1 and %s:\n" % upperBound
+
+while guess != numberToGuess:
+    guess = int(raw_input(prompt))
+    numberOfGuesses += 1
+    prompt = "Nope! Go higher...\n" if guess < numberToGuess else "Nope! Go lower...\n"
+
+print("Well done! The number was indeed %s. You took %s guesses." % (numberToGuess, numberOfGuesses))
